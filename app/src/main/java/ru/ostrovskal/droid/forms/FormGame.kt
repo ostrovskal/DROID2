839c70c053804a9d66101ef215312be1ca24452e
Сергей <ostrov_skal@mail.ru>
2018-06-01 06:17:48 +0300

    FormEditor, ViewEditor, ViewCommon. FormGame

---
+++ app/src/main/java/ru/ostrovskal/droid/forms/FormGame.kt
@@ -0,0 +1,118 @@
+package ru.ostrovskal.droid.forms
+
+import android.app.FragmentTransaction
+import android.os.Bundle
+import android.os.Message
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import com.github.ostrovskal.ssh.Constants.*
+import com.github.ostrovskal.ssh.Form
+import com.github.ostrovskal.ssh.ui.*
+import com.github.ostrovskal.ssh.utils.config
+import com.github.ostrovskal.ssh.utils.debug
+import com.github.ostrovskal.ssh.utils.send
+import com.github.ostrovskal.ssh.widgets.Text
+import ru.ostrovskal.droid.Constants.*
+import ru.ostrovskal.droid.R
+import ru.ostrovskal.droid.msg
+import ru.ostrovskal.droid.views.ViewEditor
+
+class FormGame : Form() {
+	// поверхность
+	lateinit var game: ViewEditor
+
+	private var namePlanet: Text? = null
+	
+	override fun backPressed() {
+		val tm = if(twicePressed()) tmBACK + 2000 else System.currentTimeMillis() + 10000
+		if(game.status == STATUS_LOOP) {
+			if(tm > System.currentTimeMillis()) {
+				footer(BTN_NO, 0)//, ACTION_BACKPRESSED)
+			} else {
+//				wnd.showToast(getString(R.string.press_restart), parent = content, offsX = -1, offsY = -1)
+				wnd.showToast(getString(R.string.press_restart))
+			}
+		}
+		tmBACK = System.currentTimeMillis()
+	}
+	
+	override fun twicePressed() = game.record == 0L
+	
+	override fun handleMessage(msg: Message) {
+		val s = game.surHandler
+		if(s != null) {
+			msg.apply {
+				"onMessageFormGame(what: ${what.msg} arg1: ${arg1.msg} arg2: $arg2 obj: $obj)".debug()
+				when(arg1) {
+					ACTION_LOAD     -> s.send(a1 = ACTION_LOAD, a2 = arg2)
+					ACTION_NAME     -> namePlanet?.text = obj.toString()
+					ACTION_EXIT     -> footer(BTN_NO, 0)
+					ACTION_FINISH   -> wnd.instanceForm(FORM_FINISH)
+				}
+			}
+		}
+	}
+	
+	override fun inflateContent(container: LayoutInflater): UiCtx {
+		val port = config.isVert
+		return UI {
+			linearLayout(port) {
+				containerLayout(if(port) 100 else 70, if(port) 70 else 100, true) {
+					id = R.id.gameContainer
+					game = custom {  }
+				}.lps(WRAP, WRAP)
+				cellLayout(if(port) 28 else 17, if(port) 19 else 32) {
+					backgroundSet(if(port) style_panel_port else style_panel_land)
+					repeat(3) {
+						button(style_tile_droid) { numResource = tilesGamePanel[it] }.lps(2, 6 + it * 4, 4, 4)
+						text(R.string._00000, style_text_counters).lps(6, 6 + it * 4, 5, 4)
+					}
+					val x = if(port) 11 else 1
+					val y = if(port) 6 else 19
+					repeat(2) { col ->
+						repeat(3) {
+							button(style_tile_droid) { numResource = tilesGamePanel[3 + col * 3 + it] }.lps(x + col * 8, y + it * 4, 4, 4)
+							text(R.string._000, style_text_counters).lps(x + 4 + col * 8, y + it * 4, 4, 4)
+						}
+					}
+					text(R.string.panel_text, style_text_planet) {
+						if(port) lps(1, 1, 26, 3) else lps(0, 0, 17, 6)
+					}
+				}
+			}
+		}
+	}
+
+	override fun initContent(content: ViewGroup) {
+		game.position = arguments.getInt("position")
+/*
+		game.record = arguments.getLong("record")
+		game.test = arguments.containsKey("test")
+*/
+	}
+	
+	override fun footer(btnId: Int, param: Int) {
+		if(param == ACTION_BACKPRESSED) {
+			game.surHandler?.removeMessages(STATUS_SUICIDED)
+			game.surHandler?.send(STATUS_SUICIDED, a1 = 1)
+		}
+		else super.footer(id, 0)
+	}
+	
+	override fun saveState(state: Bundle) {
+		game.saveState(state)
+		super.saveState(state)
+	}
+	
+	override fun restoreState(state: Bundle) {
+		super.restoreState(state)
+		game.restoreState(state)
+	}
+	
+	override fun setAnimation(trans: FragmentTransaction) { trans.setTransition(FragmentTransaction.TRANSIT_NONE) }
+	
+	override fun onDestroy() {
+		game.holder.removeCallback(game)
+		super.onDestroy()
+	}
+}
---
+++ app/src/main/java/ru/ostrovskal/droid/views/ViewCommon.kt
@@ -0,0 +1,273 @@
+package ru.ostrovskal.droid.views
+
+import android.content.Context
+import android.graphics.*
+import android.os.Bundle
+import android.os.Message
+import android.view.Gravity
+import android.view.SurfaceHolder
+import com.github.ostrovskal.ssh.Constants.*
+import com.github.ostrovskal.ssh.STORAGE
+import com.github.ostrovskal.ssh.StylesAndAttrs.*
+import com.github.ostrovskal.ssh.Surface
+import com.github.ostrovskal.ssh.Theme
+import com.github.ostrovskal.ssh.Touch
+import com.github.ostrovskal.ssh.utils.*
+import ru.ostrovskal.droid.Constants.*
+import ru.ostrovskal.droid.DroidWnd
+import ru.ostrovskal.droid.R
+import ru.ostrovskal.droid.msg
+import ru.ostrovskal.droid.tables.Planet
+import ru.ostrovskal.droid.tables.Stat
+import java.util.*
+
+open class ViewCommon(context: Context) : Surface(context, style_wnd)
+{
+	// превью режим
+	@STORAGE var preview				= false
+		set(v)                          { field = v; updatePreview(v, true) }
+	
+	@JvmField protected var previewMap	= Size()
+	
+	@JvmField protected var previewBlk	= Size()
+	
+	@JvmField protected var previewMO	= Point()
+	
+	@JvmField protected var previewCO	= Point()
+	
+	// признак планеты
+	val isPlanet                        get() = Planet.MAP.buffer.size > 2
+	
+	// признак воспроизведения записи
+	@STORAGE @JvmField var record		= 0L
+	
+	// параметры переходного статуса
+	@STORAGE @JvmField var newStatus	= 0
+	
+	@STORAGE @JvmField var nArg 		= 0
+	
+	@STORAGE @JvmField var idMsg 		= 0
+	
+	// признак классической игры
+	@JvmField protected val classic 	= KEY_CLASSIC.optBool
+	
+	// размер спрайта
+	@JvmField var tileBitmapSize 		= 0
+	
+	// размер спрайта на экране
+	@JvmField var tileCanvasSize 		= 0
+	
+	// позиция планеты
+	@STORAGE @JvmField var position 	= 0
+	
+	// системное сообщение
+	@STORAGE @JvmField var sysMsg 		= ""
+	
+	// статус исполнения
+	@STORAGE @JvmField var status 		= STATUS_UNK
+	
+	// ссылка на буфер
+	val buffer				            get() = Planet.MAP.buffer
+	
+	// количество ячеек в карте
+	@JvmField var countMapCells 		= 0
+	
+	// случайная величина
+	@JvmField var rnd 					= Random(System.currentTimeMillis())
+	
+	// смещение началы карты в блоках
+	@JvmField var mapOffset 			= Point()
+	
+	// смещение экрана отрисовки карты
+	@JvmField var canvasOffset 			= Point()
+	
+	// экран в фактических блоках
+	@JvmField var canvasSize 			= Size()
+	
+	// количество столбцов в тайловой карте
+	private var tilesCol 				= 0
+	
+	// экран в блоках максимум
+	private var canvasMaxSize 			= Size()
+	
+	// тайлы
+	private val tiles: Bitmap?          get() = wnd.bitmapGetCache(if(classic) "classic_sprites" else "custom_sprites")
+	
+	// ректы
+	private var canvasRect 				= Rect()
+	
+	private var bitmapRect 				= Rect()
+	
+	private var messageRect 			= RectF()
+	
+	// кисть для отрисовки сообщения
+	private var sys 					= Paint()
+	
+	// "пустая" кисть
+	private var nil 					= Paint(Paint.ANTI_ALIAS_FLAG or Paint.FILTER_BITMAP_FLAG)
+	
+	// вернуть окно
+	val wnd: DroidWnd                   get() = context as DroidWnd
+	
+	init {
+		delay = DroidWnd.applySpeed(50)
+		tileBitmapSize = (tiles?.height ?: 0) / 4
+		tilesCol = 10
+	}
+	
+	override fun restoreState(state: Bundle, vararg params: Any?) {
+		super.restoreState(state, Planet.MAP, Stat)
+	}
+
+	override fun saveState(state: Bundle, vararg params: Any?) {
+		super.saveState(state, Planet.MAP, Stat)
+	}
+	
+	protected fun initMap(calcCanvas: Boolean) {
+		Planet.MAP.apply {
+			if(isPlanet) {
+				if(calcCanvas) {
+					val mapOffs 	= Point(this@ViewCommon.width - width * tileCanvasSize,
+					                       this@ViewCommon.height - height * tileCanvasSize)
+					val canvasOffs 	= Point((this@ViewCommon.width - canvasMaxSize.w * tileCanvasSize) / 2,
+											  (this@ViewCommon.height - canvasMaxSize.h * tileCanvasSize) / 2)
+					canvasOffset.set(if(mapOffs.x < 0) canvasOffs.x else mapOffs.x / 2, if(mapOffs.y < 0) canvasOffs.y else mapOffs.y / 2)
+					canvasSize.set(if(canvasMaxSize.w > width) width else canvasMaxSize.w, if(canvasMaxSize.h > height) height else canvasMaxSize.h)
+					countMapCells = width * height
+					Touch.reset()
+					wnd.wndHandler?.send(MSG_FORM, a1 = ACTION_NAME, o = name)
+					updatePreview(preview, true)
+				}
+				val droidOffs = Point(x - canvasMaxSize.w / 2 , y - canvasMaxSize.h / 2)
+				mapOffset.set(if(droidOffs.x < 0) 0 else droidOffs.x, if(droidOffs.y < 0) 0 else droidOffs.y)
+				if(mapOffset.x + canvasSize.w > width) mapOffset.x = width - canvasSize.w
+				if(mapOffset.y + canvasSize.h > height) mapOffset.y = height - canvasSize.h
+				updatePreview(preview, false)
+			}
+		}
+	}
+	
+	override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+		super.surfaceChanged(holder, format, width, height)
+		// применить масштаб
+		tileCanvasSize = DroidWnd.applyScale((if(width > height) height else width) / (10 * config.multiplySW).toInt())
+		canvasMaxSize = Size(width / tileCanvasSize, height / tileCanvasSize)
+		messageRect.right = width.toFloat(); messageRect.bottom = height.toFloat()
+		sys.apply {
+			color = Theme.integer(context, ATTR_SSH_COLOR_MESSAGE or THEME)
+			textSize = Theme.dimen(context, R.dimen.msg).toFloat()
+			textAlign = Paint.Align.CENTER
+			typeface = context.makeFont("large")
+			setShadowLayer(Theme.dimen(context, R.dimen.shadowTextR) * 2f,
+			               Theme.dimen(context, R.dimen.shadowTextX) * 2f,
+			               Theme.dimen(context, R.dimen.shadowTextY) * 2f,
+			               0x0.color)
+		}
+	}
+	
+	override fun handleMessage(msg: Message): Boolean
+	{
+		"onMessageCommonView(what: ${msg.what.msg} arg1: ${msg.arg1.msg} arg2: ${msg.arg2} obj: ${msg.obj})".debug()
+		val h = wnd.wndHandler
+		val s = surHandler
+		if(h != null && s != null) {
+			if(wnd.restart) {
+				wnd.restart = false
+				if(newStatus != 0) s.send(STATUS_MESSAGE, 0, newStatus, nArg, idMsg)
+				else when(status) {
+					STATUS_CLEARED	-> s.send(STATUS_CLEARED, a1 = position)
+					STATUS_UNK		-> s.send(STATUS_INIT)
+					else			-> s.send(status)
+				}
+				return false
+			} else {
+				msg.apply {
+					when(what) {
+						STATUS_MESSAGE -> {
+							// изменение статуса с задержкой
+							sysMsg = resources.getString(obj as? Int ?: 0)
+							s.send(arg1, MESSAGE_DELAYED, arg2)
+						}
+						MSG_SERVICE    -> when(arg1) {
+							ACTION_LOAD     -> {
+								// загружаем карту
+								position = msg.arg2
+								canvasSize.empty()
+								val success = if(record != 0L) Stat.load(position) else Planet.MAP.load(position)
+								if(success) {
+									sysMsg = Planet.MAP.name
+									h.send(MSG_FORM, a1 = ACTION_UPDATE, a2 = position)
+									s.send(STATUS_WORK, MESSAGE_DELAYED)
+								}
+								// НЕ УДАЛОСЬ ЗАГРУЗИТЬ ПЛАНЕТУ - ВОЗМОЖНО ОНА БЫЛА ПОСЛЕДНЕЙ
+								else if(this@ViewCommon !is ViewEditor){
+									sysMsg = ""
+									h.send(MSG_FORM, a1 = ACTION_FINISH)
+								}
+							}
+							ACTION_SAVE     -> {
+								val success = Planet.MAP.store(context)
+								if(arg2 == 1) h.send(MSG_FORM, a1 = ACTION_EXIT)
+								else {
+									s.send(STATUS_MESSAGE, a1 = STATUS_WORK, o = if(success) R.string.save_planet_success else R.string.save_planet_failed)
+									// для случая свойства планеты сменили имя
+									h.send(MSG_FORM, a1 = ACTION_NAME, o = Planet.MAP.name)
+								}
+							}
+							ACTION_DELETE   -> { Planet.MAP.delete(); initMap(true) }
+							ACTION_NEW      -> { position = Planet.MAP.generator(context, arg2); h.send(MSG_FORM, a1 = ACTION_UPDATE, a2 = position); initMap(true) }
+							ACTION_GENERATE -> { Planet.default(context); h.send(MSG_FORM, a1 = ACTION_LOAD, a2 = 0) }
+						}
+						STATUS_EXIT	        -> h.send(MSG_FORM, a1 = ACTION_EXIT)
+					}
+				}
+			}
+		}
+		return super.handleMessage(msg)
+	}
+	
+	override fun draw(canvas: Canvas)
+	{
+		super.draw(canvas)
+		if(isPlanet) {
+			if(canvasSize.isEmpty()) {
+				"initMap $canvasSize $isPlanet".info()
+				initMap(true)
+			}
+			for(y in 0 until previewMap.h) {
+				canvasRect.top = previewCO.y + y * previewBlk.h
+				canvasRect.bottom = canvasRect.top + previewBlk.h
+				for(x in 0 until previewMap.w) {
+					val v = remapTiles[buffer[previewMO.x + x, previewMO.y + y] and MSKT]
+					if(v != T_NULL) {
+						bitmapRect.left = v % tilesCol * tileBitmapSize
+						bitmapRect.top = v / tilesCol * tileBitmapSize
+						bitmapRect.right = bitmapRect.left + tileBitmapSize
+						bitmapRect.bottom = bitmapRect.top + tileBitmapSize
+						canvasRect.left = previewCO.x + x * previewBlk.w
+						canvasRect.right = canvasRect.left + previewBlk.w
+						canvas.drawBitmap(tiles, bitmapRect, canvasRect, nil)
+					}
+				}
+			}
+			sys.drawTextInBounds(canvas, sysMsg, messageRect, Gravity.CENTER)
+		} else if(this is ViewEditor) {
+			sys.drawTextInBounds(canvas, context.getString(R.string.no_planet), messageRect, Gravity.CENTER)
+		}
+	}
+	
+	fun updatePreview(v: Boolean, full: Boolean) {
+		previewMO.x = if(v) 0 else mapOffset.x
+		previewMO.y = if(v) 0 else mapOffset.y
+		if(full) {
+			val h = Planet.MAP.height.toFloat()
+			val w = Planet.MAP.width.toFloat()
+			previewMap.h = if(v) h.toInt() else canvasSize.h
+			previewMap.w = if(v) w.toInt() else canvasSize.w
+			previewBlk.h = if(v) (height / h).toInt() else tileCanvasSize
+			previewBlk.w = if(v) (width / w).toInt() else tileCanvasSize
+			previewCO.x = if(v) ((width - (w * previewBlk.w)) / 2f).toInt() else canvasOffset.x
+			previewCO.y = if(v) ((height - (h * previewBlk.h)) / 2f).toInt() else canvasOffset.y
+		}
+	}
+}
